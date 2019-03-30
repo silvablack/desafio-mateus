8678fab9e146bdf8b4daaa3ada64ec66e5627677
Paulo Silva <paulosilvadev3@gmail.com>
2019-03-30 12:01:03 -0300

    make connection

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
     host: process.env.URL_DATA || 'localhost',
     port: process.env.PORT_DATA || 3306,
     user: process.env.USER_DATA || 'root',
-    password: process.env.PSW_DATA || ''
+    password: process.env.PSW_DATA || '',
+    database: process.env.DATABASE_NAME || 'estoque_mateus'
 });
 
 module.exports = connection;
